f86c2b512eb56cf10fda1b79d12b1f65d5095407
ronlv4 <ronlv4@gmail.com>
2021-04-07 15:22:14 +0300

    few fixes

--- main.py
+++ main.py
@@ -5,6 +5,28 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import warnings
 
+def findErrorForChi2overNDOFis1(function, start, end, dx):
+    errors = np.arange(start, end, dx)
+    errors_array = []
+    for error in errors:
+        for i in range(0, 6):
+            errors_array.append(error)
+    y_err = np.array(errors_array[0:6])
+    reg = Chi2Regression(linear_fun, x, y, y_err, weights=None)
+    i = 0
+    opt = Minuit(reg, a=0,b=0)
+    opt.migrad()
+    # print(reg.__call__(0.1,0.1))
+    while abs(opt.fval / reg.ndof - 1) > 0.00001 and i < len(errors_array) - 12:
+        y_err = np.array(errors_array[i:i + 6])
+        reg = Chi2Regression(linear_fun, x, y, y_err, weights=None)
+        # print(reg.__call__(0,0)/reg.ndof)
+        opt = Minuit(reg, a=0, b=0)
+        opt.migrad()
+        i += 6
+    return y_err
+
+
 df = pd.read_excel('Data.xlsx')
 y = df["y"].values
 x = df["x"].values
@@ -17,28 +39,11 @@ pol_2_fun = lambda X, a, b, c: a * X ** 2 + b * X + c
 pol_3_fun = lambda X, a, b, c, d: a * X ** 3 + b * X ** 2 + c * X + d
 sin_fun = lambda X, a, b: a * np.sin(b * X)
 exp_fun = lambda X, a, b: a * np.exp(b * X)
-errors = np.arange(0.006, 0.007, 0.00000001)
-errors_array = []
-for error in errors:
-    for i in range(0, 6):
-        errors_array.append(error)
+# y_err=findErrorForChi2overNDOFis1(linear_fun, 0.006, 0.007, 0.00000001)
+
 # define the regression,i.e. the function to be minimized, in our usual case we use Chi2Regression this is an already
 # built method to pass chi2 to Minuit as the function to be minimized
-y_err = np.array(errors_array[0:6])
 reg = Chi2Regression(linear_fun, x, y, y_err, weights=None)
-i = 0
-opt=Minuit(reg)
-opt.migrad()
-# print(reg.__call__(0.1,0.1))
-while abs(opt.fval/reg.ndof - 1) > 0.00001 and i < len(errors_array) - 12:
-    reg = Chi2Regression(linear_fun, x, y, y_err, weights=None)
-    # print(reg.__call__(0,0)/reg.ndof)
-    opt=Minuit(reg,a=0,b=0)
-    opt.migrad()
-    print(opt.fval/reg.ndof)
-    i += 6
-    y_err = np.array(errors_array[i:i + 6])
-print(y_err)
 # Chi2Regression takes the arguments (function_to_fit, x_values,y_values,y_uncertainties, weights)
 # if you are dealing with data that has no measured point by point uncertainty (i.e. you have no "y_err" in our example)
 # you need an unweighted least squares fit equivalent to weights=1 (or y_err =1 b/c 1/1^2 also equals 1)
@@ -59,7 +64,6 @@ opt = Minuit(reg)
 # where limit_a was really limit_"name" where "name" was the name of the parameter you're setting a limit on
 
 opt.migrad()  # actually do the fitting, i.e. run the optimization, i.e. minimize chi2 as defined in reg
-print(opt.fval)
 
 # The output into np arrays
 # v = opt.np_values() #the np_ methods return a np style (i.e. format) array
@@ -102,7 +106,7 @@ ax = fig.add_axes([0.1, 0.15, .85, .77])
 # L B R T
 ax.set_xlabel(r'm - Number of Rings', fontdict={"size": 20, "weight": "bold"})
 ax.set_ylabel(r'x - Micrometer Reading [mm]', fontdict={"size": 20, "weight": "bold"})
-ax.set_title("Rotation Distance as a Function of Rings Created", fontdict={"size": 24, "weight": "bold"})
+ax.set_title("Rotation Distance as Function of Rings Created", fontdict={"size": 24, "weight": "bold"})
 ax.errorbar(x=x, y=y, yerr=y_err, xerr=x_err, capsize=4, elinewidth=3, fmt='none', ecolor="blue")
 ax.scatter(x, y, c='blue', s=30)
 reg.show(minuit=None, errors=opt.errors)
