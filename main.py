349fa65ca60de6613f97279d9dbe92ff824a3d29
ronlv4 <ronlv4@gmail.com>
2021-04-11 13:54:20 +0300

    added gaussian

--- main.py
+++ main.py
@@ -5,25 +5,26 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import warnings
 
-def findErrorForChi2overNDOFis1(function, start, end, dx):
+def findErrorForChi2overNDOFis1(function, start, end, dx,err_size):
     errors = np.arange(start, end, dx)
     errors_array = []
     for error in errors:
-        for i in range(0, 6):
+        for i in range(0, err_size):
             errors_array.append(error)
-    y_err = np.array(errors_array[0:6])
+    y_err = np.array(errors_array[0:err_size])
     reg = Chi2Regression(linear_fun, x, y, y_err, weights=None)
     i = 0
     opt = Minuit(reg, a=0,b=0)
     opt.migrad()
     # print(reg.__call__(0.1,0.1))
-    while abs(opt.fval / reg.ndof - 1) > 0.00001 and i < len(errors_array) - 12:
-        y_err = np.array(errors_array[i:i + 6])
+    while abs(opt.fval / reg.ndof - 1) > 0.1 and i < len(errors_array) - 12:
+        y_err = np.array(errors_array[i:i + err_size])
         reg = Chi2Regression(linear_fun, x, y, y_err, weights=None)
         # print(reg.__call__(0,0)/reg.ndof)
         opt = Minuit(reg, a=0, b=0)
         opt.migrad()
-        i += 6
+        i += err_size
+    print(y_err)
     return y_err
 
 
@@ -39,7 +40,8 @@ pol_2_fun = lambda X, a, b, c: a * X ** 2 + b * X + c
 pol_3_fun = lambda X, a, b, c, d: a * X ** 3 + b * X ** 2 + c * X + d
 sin_fun = lambda X, a, b: a * np.sin(b * X)
 exp_fun = lambda X, a, b: a * np.exp(b * X)
-# y_err=findErrorForChi2overNDOFis1(linear_fun, 0.006, 0.007, 0.00000001)
+gaussian = lambda X, a, e, s: a*np.exp(-((X-e)**2)/(2*s**2))
+# y_err=findErrorForChi2overNDOFis1(linear_fun, 0.0250454, 0.0250455, 0.00000000001, len(x))
 
 # define the regression,i.e. the function to be minimized, in our usual case we use Chi2Regression this is an already
 # built method to pass chi2 to Minuit as the function to be minimized
@@ -104,9 +106,9 @@ plt.rc("font", size=16, family="Times New Roman")
 fig = plt.figure(figsize=(10, 6))
 ax = fig.add_axes([0.1, 0.15, .85, .77])
 # L B R T
-ax.set_xlabel(r'm - Number of Rings', fontdict={"size": 20, "weight": "bold"})
-ax.set_ylabel(r'x - Micrometer Reading [mm]', fontdict={"size": 20, "weight": "bold"})
-ax.set_title("Rotation Distance as Function of Rings Created", fontdict={"size": 24, "weight": "bold"})
+ax.set_xlabel(r'Number of Rings', fontdict={"size": 20, "weight": "bold"})
+ax.set_ylabel(r'Pressure [mmHg]', fontdict={"size": 20, "weight": "bold"})
+ax.set_title("Pressure as a Function of Rings Created", fontdict={"size": 24, "weight": "bold"})
 ax.errorbar(x=x, y=y, yerr=y_err, xerr=x_err, capsize=4, elinewidth=3, fmt='none', ecolor="blue")
-ax.scatter(x, y, c='blue', s=30)
-reg.show(minuit=None, errors=opt.errors)
+ax.scatter(x, y, c='blue', s=10)
+reg.show(minuit=None, parmloc=(0.7,0.95), errors=opt.errors)
