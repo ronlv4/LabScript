e7cbf601b5c9be70c4e083d332dd3f04f19b574e
ronlv4 <ronlv4@gmail.com>
2021-05-30 18:25:50 +0300

    ..

--- simple_plot.py
+++ simple_plot.py
@@ -3,8 +3,12 @@ import numpy as np
 import pandas as pd
 
 df = pd.read_excel(r'Data.xlsx')
-measured_x = df['x'].values
-measured_y = df['y'].values
+x = df['x'].values
+y = df['y'].values
+x2 = df['x2'].values
+y2 = df['y2'].values
+x3 = df['x3'].values
+y3 = df['y3'].values
 # x_uncertainties = np.full_like(measured_x, df['x_err'].values[0], float)
 # y_uncertainties = np.full_like(measured_y, df['y_err'].values[0], float)
 x_uncertainties = df['x_err'].values
@@ -14,12 +18,18 @@ y_uncertainties = df['y_err'].values
 # ax.legend()
 # plt.figure(1, figsize=(10, 6))
 marker_color = 'blue'
-line_color = 'brown'
+line_color = 'red'
+marker_color2 = 'blue'
+line_color2 = 'gray'
+marker_color3 = 'blue'
+line_color3 = 'brown'
 plt.figure(figsize=(10,6))
-plt.plot(measured_x, measured_y,color= line_color,marker='.', label='540 [nm]', markeredgecolor=marker_color, markerfacecolor=marker_color)
+plt.plot(x2,y2,color= line_color2,marker='.', label='450 [nm]', markeredgecolor=marker_color2, markerfacecolor=marker_color2)
+plt.plot(x,y,color= line_color,marker='.', label='540 [nm]', markeredgecolor=marker_color, markerfacecolor=marker_color)
+plt.plot(x3,y3,color= line_color3,marker='.', label='602 [nm]', markeredgecolor=marker_color3, markerfacecolor=marker_color3)
 plt.grid()
-# plt.legend()
+plt.legend()
 plt.xlabel('Voltage [V]')
 plt.ylabel('Current [mA]')
-plt.title('I-V Characteristic - filter 602 [nm]')
+plt.title('I-V Characteristic - all filters')
 plt.show()
